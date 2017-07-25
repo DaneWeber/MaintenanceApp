d0efff6859fe6345e536bc94bfdafc87cd6ac83c
Fluffums <github.19.denmarx@spamgourmet.com>
2017-07-25 11:44:43 -0400

    Fix date logic and color scheme.

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,19 +2,17 @@ class Chore < ApplicationRecord
   validates :name, presence: true
   validates :interval_days, numericality: { only_integer: true, greater_than: 0 }
 
-  def due
-    # 'due-today'
-    # next_due
+  def due_class
     if next_due == nil
-      'nil'
-    elsif next_due == Date.today
-      'due-today'
+      'not-due'
     elsif next_due < Date.today
       'overdue'
-    elsif next_due > Date.today - 7
+    elsif next_due < Date.today + 1
+      'due-today'
+    elsif next_due < Date.today + 7
       'due-week'
     else
-      'not-due'
+      'due-later'
     end
   end
 end
