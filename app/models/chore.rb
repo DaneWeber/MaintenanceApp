2ea40e51d06990dc3164f90546af1a0268902a1b
Fluffums <github.19.denmarx@spamgourmet.com>
2017-07-25 11:24:38 -0400

    Color rows based on when due.

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,20 @@
 class Chore < ApplicationRecord
   validates :name, presence: true
   validates :interval_days, numericality: { only_integer: true, greater_than: 0 }
+
+  def due
+    # 'due-today'
+    # next_due
+    if next_due == nil
+      'nil'
+    elsif next_due == Date.today
+      'due-today'
+    elsif next_due < Date.today
+      'overdue'
+    elsif next_due > Date.today - 7
+      'due-week'
+    else
+      'not-due'
+    end
+  end
 end
