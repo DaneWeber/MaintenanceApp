3ce98d9c903aa0a292ed5ca1bc8a1e91ed1d8f83
Fluffums <github.19.denmarx@spamgourmet.com>
2017-07-25 13:00:53 -0400

    Switch to case statement.

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -3,13 +3,14 @@ class Chore < ApplicationRecord
   validates :interval_days, numericality: { only_integer: true, greater_than: 0 }
 
   def due_class
-    if next_due == nil
+    case
+    when next_due == nil
       'not-due'
-    elsif next_due < Date.today
+    when next_due < Date.today
       'overdue'
-    elsif next_due < Date.today + 1
+    when next_due < Date.today + 1
       'due-today'
-    elsif next_due < Date.today + 7
+    when next_due < Date.today + 7
       'due-week'
     else
       'due-later'
