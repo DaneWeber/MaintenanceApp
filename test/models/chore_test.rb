3072b72a8d16100cf5d7d7b4f939caf2968037cf
Fluffums <github.19.denmarx@spamgourmet.com>
2017-07-26 11:11:04 -0400

    Loop assertions to test dates

--- test/models/chore_test.rb
+++ test/models/chore_test.rb
@@ -5,41 +5,26 @@ class ChoreTest < ActiveSupport::TestCase
   #   assert true
   # end
   test 'due_class calculations' do
-    # it 'should be not-due when there is no due date' do
-      # Arrange
-      test_chore = Chore.new
-      # Act
-      # case
-      # when next_due == nil
-      #   'not-due'
-      # when next_due < Date.today
-      #   'overdue'
-      # when next_due < Date.today + 1
-      #   'due-today'
-      # when next_due < Date.today + 7
-      #   'due-week'
-      # else
-      #   'due-later'
-      # end
+    # Arrange
+    test_chore = Chore.new
 
-      # Assert
-      assert_equal('not-due',   test_chore.due_class(nil),             'A blank due_date should be not-due')
-      assert_equal('due-later', test_chore.due_class(Date.today + 10), 'Ten days in the future should be due-later')
-      assert_equal('due-later', test_chore.due_class(Date.today + 9),  'Nine days in the future should be due-later')
-      assert_equal('due-later', test_chore.due_class(Date.today + 8),  'Eight days in the future should be due-later')
-      assert_equal('due-later', test_chore.due_class(Date.today + 7),  'Seven days in the future should be due-later')
-      assert_equal('due-week',  test_chore.due_class(Date.today + 6),  'Six days in the future should be due-week')
-      assert_equal('due-week',  test_chore.due_class(Date.today + 5),  'Five days in the future should be due-week')
-      assert_equal('due-week',  test_chore.due_class(Date.today + 4),  'Four days in the future should be due-week')
-      assert_equal('due-week',  test_chore.due_class(Date.today + 3),  'Three days in the future should be due-week')
-      assert_equal('due-week',  test_chore.due_class(Date.today + 2),  'Two days in the future should be due-week')
-      assert_equal('due-week',  test_chore.due_class(Date.today + 1),  'One day in the future should be due-week')
-      assert_equal('due-today', test_chore.due_class(Date.today + 0),  'Today should be due-today')
-      assert_equal('overdue',   test_chore.due_class(Date.today - 1),  'One day in the future should be overdue')
-      assert_equal('overdue',   test_chore.due_class(Date.today - 2),  'Two days in the future should be overdue')
-      assert_equal('overdue',   test_chore.due_class(Date.today - 3),  'Three days in the future should be overdue')
-      assert_equal('overdue',   test_chore.due_class(Date.today - 4),  'Four days in the future should be overdue')
-      assert_equal('overdue',   test_chore.due_class(Date.today - 5),  'Five days in the future should be overdue')
-    # end
+    # Act
+
+    # Assert
+    assert_equal('not-due',   test_chore.due_class(nil),             'A blank due_date should be not-due')
+
+    (7..20).each do |n|
+      assert_equal('due-later', test_chore.due_class(Date.today + n), "#{n} days in the future should be due later than this week.")
+    end
+
+    (1..6).each do |n|
+      assert_equal('due-week', test_chore.due_class(Date.today + n), "#{n} days in the future should be due this week, but not today.")
+    end
+
+    assert_equal('due-today', test_chore.due_class(Date.today),  'Today should be due-today')
+
+    (-10..-1).each do |n|
+      assert_equal('overdue', test_chore.due_class(Date.today + n), "When due in the past (#{0 - n} days) it should be overdue")
+    end
   end
 end
