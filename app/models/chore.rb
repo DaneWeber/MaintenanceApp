abfe67cc1497768baaaf8a56aed3b9d6cf3dd512
DaneWeber <github.19.denmarx@spamgourmet.com>
2017-07-31 17:37:51 -0400

    Move due date calculation into its own function

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -7,18 +7,19 @@ class Chore < ApplicationRecord
   def reset_cycle_date
     self.last_done = Date.today
     self.cycle_reset = Time.now
-    case
-    when calendar_days?
-      self.next_due = Date.today + self.interval_days
-    when business_days?
-      self.next_due = self.add_business_days(start_date: Date.today, work_days: self.interval_days)
-    else
-      return false
-    end
-    true
+    self.next_due = calculate_next_due()
   end
 
   # Functions
+  def calculate_next_due
+    case
+    when calendar_days?
+      Date.today + self.interval_days
+    when business_days?
+      self.add_business_days(start_date: Date.today, work_days: self.interval_days)
+    end
+  end
+
   def due_class(due_date = next_due)
     raise ArgumentError, 'nil or date required' unless due_date.instance_of?(Date) || due_date.nil?
 
