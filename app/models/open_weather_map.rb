db5f55917c073d8d36673e018a6cae8328ac50c7
DaneWeber <github.19.denmarx@spamgourmet.com>
2017-08-08 15:35:16 -0400

    Hit mock API

--- app/models/open_weather_map.rb
+++ app/models/open_weather_map.rb
@@ -1,11 +1,27 @@
 COOLDOWN_IN_MINUTES = 10
-OPEN_WEATHER_MAP_URI = 'https://home.openweathermap.org/'
+OPEN_WEATHER_MAP_URI = 'http://api.openweathermap.org/'
+OPEN_WEATHER_MAP_PATH = 'data/2.5/forecast/daily'
+OPEN_WEATHER_MAP_CRITERIA = 'zip=22630,us'
 OPEN_WEATHER_MAP_KEY = '3fcb4e23443750cc1256f3016a160ca7'
+OPEN_WEATHER_MAP_FULL_URL = OPEN_WEATHER_MAP_URI + OPEN_WEATHER_MAP_PATH + '?' + OPEN_WEATHER_MAP_CRITERIA + '&appid=' + OPEN_WEATHER_MAP_KEY
 
 class OpenWeatherMap < ApplicationRecord
   validates :last_get, presence: true
 
   def current_weather_payload
+    self.get_weather if OpenWeatherMap.safe_to_poll(DateTime.now)
+    response
+  end
+
+  def get_weather
+    api_response = HTTParty.get(OPEN_WEATHER_MAP_FULL_URL)
+    if api_response.success?
+      self.response = api_response.body
+      self.api_key = OPEN_WEATHER_MAP_KEY
+      self.criteria = OPEN_WEATHER_MAP_CRITERIA
+      self.last_get = DateTime.now
+      self.save
+    end
   end
 
   def self.latest_get
--- test/integration/open_weather_map_test.rb
+++ test/integration/open_weather_map_test.rb
@@ -1,4 +1,4 @@
-OPEN_WEATHER_MAP_URI = 'https://home.openweathermap.org/'
+OPEN_WEATHER_MAP_STUB = 'http://api.openweathermap.org/data/2.5/forecast/daily?appid=3fcb4e23443750cc1256f3016a160ca7&zip=22630,us'
 
 require 'test_helper'
 require 'webmock/minitest'
@@ -6,12 +6,12 @@ require 'webmock/minitest'
 class OpenWeatherMapIntTest < ActionDispatch::IntegrationTest
   test "call Open Weather Map API if safe" do
     stub_body_text = "Stub created at #{DateTime.now} for #{OPEN_WEATHER_MAP_URI} url."
-    stub_request(:any, OPEN_WEATHER_MAP_URI)
+    stub_request(:any, OPEN_WEATHER_MAP_STUB)
       .to_return(body: stub_body_text)
-    response = HTTParty.get(OPEN_WEATHER_MAP_URI)
+    response = HTTParty.get(OPEN_WEATHER_MAP_STUB)
     assert_equal(200, response.code, 'expect successful stub hit')
-    test_api = OpenWeatherMap.new
+    test_api = OpenWeatherMap.new(response: 'empty')
     assert(OpenWeatherMap.safe_to_poll(DateTime.now), 'fixtures should be safely in the past')
-    # assert_equal(stub_body_text, test_api.current_weather_payload, 'should call the API')
+    assert_equal(stub_body_text, test_api.current_weather_payload, 'should call the API')
   end
 end
